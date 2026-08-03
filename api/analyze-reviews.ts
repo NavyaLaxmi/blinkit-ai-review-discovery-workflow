02b4cf1734c9b1bef12d17ffedee0b1af8c43020
NavyaLaxmi <sirikonda.navyalaxmi@capgemini.com>
2026-08-04 03:49:21 +0530

    Fix import extension

--- api/analyze-reviews.ts
+++ api/analyze-reviews.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { analyzeReviewsWithGemini } from "../src/utils/geminiAnalyzer";
+import { analyzeReviewsWithGemini } from "../src/utils/geminiAnalyzer.ts";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Set CORS headers
